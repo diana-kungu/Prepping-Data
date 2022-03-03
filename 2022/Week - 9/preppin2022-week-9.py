4876052d3c650e1c74c7cacb1db6cb85d12ecda0
Diana Kung'u <dkungu@gmail.com>
2022-03-03 08:15:09 +0300

    Readme.md updated

--- 2022/Week - 9/preppin2022-week-9.py
+++ 2022/Week - 9/preppin2022-week-9.py
@@ -1,20 +1,18 @@
-# %%
+# Load Libraries
 from pandas import read_csv, Series, merge
 from numpy import where, diff
 
-# %%
-#READ DATA
-df = read_csv(r'.\Input\Superstore - Order.csv', parse_dates=['Order Date', 'Ship Date'])
-df.head(2)
 
-# %%
+#READ DATA
+df = read_csv(r'.\2022\Week - 9\\Input\Superstore - Order.csv', parse_dates=['Order Date', 'Ship Date'])
+
+#PROCESS DATA
 #Order year 
 df['Year'] = df['Order Date'].dt.year
 #Find the year of first purchase for each customer
 df['First Purchase']  = df.groupby(['Customer ID'])['Year'].transform('min')
-df.head(2)
 
-# %%
+# 
 current_yr = df['Year'].max()
 
 for key, group in df.groupby('Customer ID'):
@@ -23,16 +21,12 @@ for key, group in df.groupby('Customer ID'):
             df = df.append({'Year': yr, 'Customer ID': group['Customer ID'].iloc[0],
                             'First Purchase': group['First Purchase'].min()}, ignore_index=True)
 
-
-# %%
 #Create a field which flags whether a customer placed an order in a given year or not
 df['Order?'] = where(df['Row ID'].isna(), 0,1)
 
 #Create a field which flags whether or not a customer placed an order in the previous year
 df['Prev_Value'] = (df.groupby(['Customer ID'])['Order?'].transform(lambda x: x.shift()))
 
-
-# %%
 '''classify the customers as follows:
     New = this is the first year the customer has ordered
     Consistent = the customer ordered this year and last year
@@ -45,19 +39,13 @@ df['Customer Classification'] = where(df['Prev_Value'].isna(), 'New',
                                       'Sleeping'          
                                             )))
 
-# %%
+# 2018 Cohort: count number of purchases per year for the 2018 cohort
 cohort_18 = df[(df['First Purchase'] == 2018)].groupby(['Year'], as_index= False).apply(lambda x: Series({
     'YoY Difference': x[x['Order?'] == 1]['Customer ID'].nunique()}))
 cohort_18['YoY Difference'] = cohort_18['YoY Difference'].rolling(window=2).apply(diff)
-cohort_18
 
-# %%
 df = df.merge(cohort_18)
 
-# %%
-df[df['Customer ID'] =='TB-21190']
-
-# %% [markdown]
-# 
-
-
+#OUTPUT
+df.to_csv(r'.\2022\Week - 9\Output\week-9-output.csv', index = False)
+print('End')
